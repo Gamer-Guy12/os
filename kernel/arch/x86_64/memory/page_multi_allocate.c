772ed7c302b9e6454fe9f0db2d751ebc515e6e65
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-04-16 03:55:20 +0000

    Added multi page allocation

---
+++ kernel/arch/x86_64/memory/page_multi_allocate.c
@@ -0,0 +1,9 @@
+#include <libk/bit.h>
+#include <pmm.h>
+#include <stddef.h>
+
+void allocate_page_frames(pageframe_t *pages, uint64_t count) {
+  for (size_t i = 0; i < count; i++) {
+    pages[i] = allocate_page_frame();
+  }
+}
--- kernel/include/pmm.h
+++ kernel/include/pmm.h
@@ -8,8 +8,8 @@
 typedef uint8_t *pageframe_t;
 
 void setup_page_frame_allocation(uint8_t *multiboot);
-/// By default this will allocate above 16 MB
 pageframe_t allocate_page_frame(void);
+void allocate_page_frames(pageframe_t *pages, uint64_t count);
 void free_page_frame(pageframe_t *page);
 
 #endif
