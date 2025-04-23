dc9a3d8aa066b2436480baf74e56d80fe57df2bd
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-04-23 04:00:26 +0000

    Decided to remove a lot of cores startup cuz i need to work on the hal so that i can do interuppts

--- kernel/arch/x86_64/start_cores.c
+++ kernel/arch/x86_64/start_cores.c
@@ -27,103 +27,13 @@ static inline void wait_ms(uint32_t ms) {
   }
 }
 
+static lock_t starting_cores;
+
 void start_cores(uint8_t *multiboot) {
-  lock_t starting_cores;
 
   lock_acquire(&starting_cores);
 
-  XSDP_t *xsdp = find_xsdp(multiboot);
-  XSDT_t *xsdt = find_xsdt(xsdp);
-  MADT_t *madt = find_madt(xsdt);
-  uint8_t core_count = get_cores(NULL, madt);
-  uint8_t cores[core_count];
-  get_cores(cores, madt);
-  volatile uint32_t *lapic_addr = (uint32_t *)(uint64_t)madt->lapic_addr;
-
-  kio_printf("The lapic addr is %u\n", (uint64_t)lapic_addr);
-
-  uint8_t bspid = 0;
-  __asm__ __volatile__("mov $1, %%eax; cpuid; shrl $24, %%ebx;"
-                       : "=b"(bspid)
-                       :
-                       :);
-
-  kio_printf("The bspid is %u", (uint64_t)bspid);
-
-  wait_ms(1000);
-
-  // memcpy(&core_count, (void *)0x8000, 1);
-  memcpy(&ap_tramp, (void *)0x8000, ap_len);
-
-  wait_ms(1000);
-
-  // for (size_t i = 0; i < core_count; i++) {
-  //   if (cores[i] == bspid)
-  //     continue;
-  //
-  //   lapic_addr[0x280] = 0;
-  //   lapic_addr[0x310] = (lapic_addr[0x310] & 0x00FFFFFF) | cores[i] << 24;
-  //   lapic_addr[0x300] = 0x00004500;
-  //   wait_ms(1000);
-  //   do {
-  //     __asm__ __volatile__("pause" : : : "memory");
-  //   } while (!(lapic_addr[0x300] & 1 << 12));
-  //   wait_ms(1000);
-  //
-  //   for (size_t j = 0; j < 2; j++) {
-  //     lapic_addr[0x280] = 0;
-  //     lapic_addr[0x310] = (lapic_addr[0x310] & 0x00FFFFFF) | cores[i] << 24;
-  //     lapic_addr[0x300] = 0x00004600 | 8;
-  //     wait_ms(1);
-  //     do {
-  //       __asm__ __volatile__("pause" : : : "memory");
-  //     } while (!(lapic_addr[0x300] & 1 << 12));
-  //   }
-  // }
-
-  for (size_t i = 0; i < core_count; i++) {
-    // do not start BSP, that's already running this code
-    if (cores[i] == bspid)
-      continue;
-    // send INIT IPI
-    *((volatile uint32_t *)(lapic_addr + 0x280)) = 0; // clear APIC errors
-    *((volatile uint32_t *)(lapic_addr + 0x310)) =
-        (*((volatile uint32_t *)(lapic_addr + 0x310)) & 0x00ffffff) |
-        (i << 24); // select AP
-    *((volatile uint32_t *)(lapic_addr + 0x300)) =
-        (*((volatile uint32_t *)(lapic_addr + 0x300)) & 0xfff00000) |
-        0x00C500; // trigger INIT IPI
-    do {
-      __asm__ __volatile__("pause" : : : "memory");
-    } while (*((volatile uint32_t *)(lapic_addr + 0x300)) &
-             (1 << 12)); // wait for delivery
-    *((volatile uint32_t *)(lapic_addr + 0x310)) =
-        (*((volatile uint32_t *)(lapic_addr + 0x310)) & 0x00ffffff) |
-        (i << 24); // select AP
-    *((volatile uint32_t *)(lapic_addr + 0x300)) =
-        (*((volatile uint32_t *)(lapic_addr + 0x300)) & 0xfff00000) |
-        0x008500; // deassert
-    do {
-      __asm__ __volatile__("pause" : : : "memory");
-    } while (*((volatile uint32_t *)(lapic_addr + 0x300)) &
-             (1 << 12)); // wait for delivery
-    wait_ms(10); // wait 10 msec
-    // send STARTUP IPI (twice)
-    for (size_t j = 0; j < 2; j++) {
-      *((volatile uint32_t *)(lapic_addr + 0x280)) = 0; // clear APIC errors
-      *((volatile uint32_t *)(lapic_addr + 0x310)) =
-          (*((volatile uint32_t *)(lapic_addr + 0x310)) & 0x00ffffff) |
-          (i << 24); // select AP
-      *((volatile uint32_t *)(lapic_addr + 0x300)) =
-          (*((volatile uint32_t *)(lapic_addr + 0x300)) & 0xfff0f800) |
-          0x000608; // trigger STARTUP IPI for 0800:0000
-      wait_ms(1);   // wait 200 usec
-      do {
-        __asm__ __volatile__("pause" : : : "memory");
-      } while (*((volatile uint32_t *)(lapic_addr + 0x300)) &
-               (1 << 12)); // wait for delivery
-    }
-  }
+  
 
   lock_release(&starting_cores);
 }
---
+++ kernel/include/HAL/hal.h
@@ -0,0 +1,6 @@
+#ifndef HAL_H
+#define HAL_H
+
+void init_hal(void);
+
+#endif
