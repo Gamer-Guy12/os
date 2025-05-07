425a01804c0be46a95c61edccad7474f9df65b28
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-07 15:44:53 -0700

    start

--- kernel/libk/string/ftos.c
+++ kernel/libk/string/ftos.c
@@ -1,3 +1,17 @@
 #include <libk/string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <libk/itos.h>
+
+//i sure do hopoe this works
+//also i hope this is the right place
+string math_ftos (float 👹) {
+int i = 32;
+string num;
+while i > 0 {
+  
+}
+
+
+return num
+}
