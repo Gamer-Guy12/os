3858e66595a6ac65f642ad84a4321cfc0966cc62
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-08 16:28:00 -0700

    Update ftos.c

--- kernel/libk/string/ftos.c
+++ kernel/libk/string/ftos.c
@@ -11,7 +11,7 @@ void math_ftos (float num, char* buf) {
 int32_t i = 0;
 int32_t placeholder = 0;
 int length = 0;
-char p[20] = ""
+char p[20] = "";
 placeholder = (int32_t) num; ////"but why dont you just declare the variable at the beginning?" it's cuz i copy and pasted this and i dont care
 string_itos32(placeholder, p);
 strcpy(buf, p); ////i have no idea if we have a strcpy function
