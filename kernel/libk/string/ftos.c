9a5554fdae3e55fd88ccf410d7282f448a2a631a
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-07 17:30:29 -0700

    Update ftos.c
    ghggguuuuhhhh

--- kernel/libk/string/ftos.c
+++ kernel/libk/string/ftos.c
@@ -6,16 +6,13 @@
 ///i sure do hopoe this works
 ///also i hope this is the right place
 void math_ftos (float num, char* buf) {
-int i = 0;
-int placeholder = 0;
+int32_t i = 0;
+int32_t placeholder = 0;
 while (i < 32) {
-  int placeholder = int(buf)
-  buf[i] = placeholder;
+  placeholder = (int32_t) num;
+  buf = buf + string_itos32(placeholder);
   num = num - placeholder;
   num = num * 10;
   i++;
 }
-
-
-return num
 }
