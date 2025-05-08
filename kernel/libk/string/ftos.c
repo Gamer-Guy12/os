5fe7c67b1cd63d09545f9eb2a0642fe846f931b4
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-08 11:22:02 -0700

    almost finished

--- kernel/libk/string/ftos.c
+++ kernel/libk/string/ftos.c
@@ -11,14 +11,14 @@ void math_ftos (float num, char* buf) {
 int32_t i = 0;
 int32_t placeholder = 0;
 int length = 0;
-char* p = ""
+char p[20] = ""
 placeholder = (int32_t) num; ////"but why dont you just declare the variable at the beginning?" it's cuz i copy and pasted this and i dont care
 string_itos32(placeholder, p);
-buf = p;
-length = length(p) + 1;
+strcpy(buf, p); ////i have no idea if we have a strcpy function
+length = strlen(p) + 1;
 num = num - placeholder;
 num = num * 10;
-*buf = buf + '.'; //decimal point!
+buf[length++] = '.'; //decimal point!
 while (length < 20) { //this is wayyy more digits than we need and/or is accurate.
   placeholder = (int32_t) num;
   string_itos32(placeholder, p);
@@ -27,5 +27,5 @@ while (length < 20) { //this is wayyy more digits than we need and/or is accurat
   num = num * 10;
   length++;
 }
-buf[length] = '\0'
+buf[length] = '\0';
 }
