d303cf873b843182870e4bf15a526cab0e781048
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-08 09:24:32 -0700

    guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?
    guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?guh?

--- kernel/libk/string/ftos.c
+++ kernel/libk/string/ftos.c
@@ -8,12 +8,16 @@
 void math_ftos (float num, char* buf) {
 int32_t i = 0;
 int32_t placeholder = 0;
+int length = 0;
+char* p = ""
 placeholder = (int32_t) num; ////"but why dont you just declare the variable at the beginning?" it's cuz i copy and pasted this and i dont care
-*buf = buf + string_itos32(placeholder);
+string_itos32(placeholder, p);
+buf = p;
+length = length(p);
 num = num - placeholder;
 num = num * 10;
 *buf = buf + '.'; //decimal point!
-while (i < 10) { //10 digit accuracy. floats aren't really accurate after this. if you don't like this cry about it
+while (length < 20) { //this is wayyy more digits than we need and/or is accurate.
   placeholder = (int32_t) num;
   buf = buf + string_itos32(placeholder);
   num = num - placeholder;
