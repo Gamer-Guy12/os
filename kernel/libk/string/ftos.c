4ae4323be60a7c063a6d21b8e1d692a3766de900
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-07 17:53:37 -0700

    Update ftos.c

--- kernel/libk/string/ftos.c
+++ kernel/libk/string/ftos.c
@@ -8,7 +8,12 @@
 void math_ftos (float num, char* buf) {
 int32_t i = 0;
 int32_t placeholder = 0;
-while (i < 32) {
+placeholder = (int32_t) num; ////"but why dont you just declare the variable at the beginning?" it's cuz i copy and pasted this and i dont care
+buf = buf + string_itos32(placeholder);
+num = num - placeholder;
+num = num * 10;
+buf = buf + "." //decimal point!
+while (i < 10) { //10 digit accuracy. floats aren't really accurate after this. if you don't like this cry about it
   placeholder = (int32_t) num;
   buf = buf + string_itos32(placeholder);
   num = num - placeholder;
