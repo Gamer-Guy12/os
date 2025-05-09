188be2a8f2484bc08603cb878073a7ce12f0c968
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-08 21:32:56 -0700

    puh puh puh puh puh puh puh puh puh puh puh puh?

---
+++ kernel/libk/string/strcpy.c
@@ -0,0 +1,11 @@
+//// how has this not be made
+#include <libk/string.h>
+#include <libk/strlen.h>
+
+void strcpy (char* og, char* notog) {
+  uint16_t i = 0
+  while ( i < strlen(string1)) {
+    notog[i]=og[i];
+    i++;    
+  }
+}
