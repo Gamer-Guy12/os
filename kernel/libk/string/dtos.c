1d7a756a3dd8446a4a9713ae9356d3d0988c8f71
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-09 09:30:15 -0700

    Update dtos.c

--- kernel/libk/string/dtos.c
+++ kernel/libk/string/dtos.c
@@ -1,1 +1,31 @@
+#include <libk/string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <libk/itos.h>
+#include <libk/stcmp.h>
+#include <libk/strlen.h>
 
+///i sure do hopoe this works
+///also i hope this is the right place
+void string_dtos (double num, char* buf) {
+int32_t i = 0;
+int32_t placeholder = 0;
+int length = 0;
+char p[20] = "";
+placeholder = (int32_t) num; ////"but why dont you just declare the variable at the beginning?" it's cuz i copy and pasted this and i dont care
+string_itos32(placeholder, p);
+strcpy(buf, p);
+length = strlen(p);
+num = num - placeholder;
+num = num * 10;
+buf[length++] = '.'; //decimal point!
+while (length < 32) { //this is wayyy more digits than we need and/or is accurate.
+  placeholder = (int32_t) num;
+  string_itos32(placeholder, p);
+  buf[length] = p[0];
+  num = num - placeholder;
+  num = num * 10;
+  length++;
+}
+buf[length] = '\0';
+}
