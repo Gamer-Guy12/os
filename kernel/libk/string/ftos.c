b9834e30724fc2b868467362eaafdfb5f1529880
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-09 09:26:00 -0700

    Update ftos.c

--- kernel/libk/string/ftos.c
+++ kernel/libk/string/ftos.c
@@ -14,7 +14,7 @@ int length = 0;
 char p[20] = "";
 placeholder = (int32_t) num; ////"but why dont you just declare the variable at the beginning?" it's cuz i copy and pasted this and i dont care
 string_itos32(placeholder, p);
-strcpy(buf, p);
+strcpy(p, buf);
 length = strlen(p);
 num = num - placeholder;
 num = num * 10;
