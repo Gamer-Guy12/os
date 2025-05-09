f3de577c36cb395872b2f20889b2b3c4fef955d2
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-05-09 09:24:21 -0700

    ok

--- kernel/libk/string/strcpy.c
+++ kernel/libk/string/strcpy.c
@@ -1,11 +1,8 @@
-//// how has this not be made
 #include <libk/string.h>
-#include <libk/strlen.h>
 
 void strcpy (char* src, char* dest) {
-  uint16_t i = 0;
+  size_t i = 0;
   do {
-    src[i] = dest[i];
-    i++;
-  } while (src[i] != '\0');
+    dest[i] = src[i];
+  } while (src[i++] != '\0');
 }
