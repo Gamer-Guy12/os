18a39f8ea763e0ab3a9e0b1b0ef614f5b77c0298
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-05-13 00:33:08 +0000

    Page tables are working other than the permissions: gotta fix thos

--- kernel/arch/x86_64/hal/memory/init_memory_manager.c
+++ kernel/arch/x86_64/hal/memory/init_memory_manager.c
@@ -1,5 +1,7 @@
+#include <asm.h>
 #include <hal/imemory.h>
 #include <hal/memory.h>
+#include <libk/bit.h>
 #include <libk/kio.h>
 #include <libk/lock.h>
 #include <libk/math.h>
@@ -149,7 +151,8 @@ static void create_page_tables(void) {
   pml4[511].not_executable = 0;
   pml4[511].full_entry |= PAGE_ADDR(pdpt);
 
-  kio_printf("PML4: index 0, val %x\n", pml4[511].full_entry);
+  kio_printf("PML4: index 0, val %x, ptr %x\n", pml4[511].full_entry,
+             (size_t)pdpt - KERNEL_OFFSET);
 
   // Save 2 pages (hopefully contigouous)
   // Should be cuz no other concurrent things
@@ -165,8 +168,10 @@ static void create_page_tables(void) {
   pdpt[511].not_executable = 0;
   pdpt[511].full_entry |= PAGE_ADDR(&pdt[512]);
 
-  kio_printf("PDPT: index 0, val %x\n", pdpt[510].full_entry);
-  kio_printf("PDPT: index 1, val %x\n", pdpt[511].full_entry);
+  kio_printf("PDPT: index 0, val %x, ptr %x\n", pdpt[510].full_entry,
+             (size_t)pdt - KERNEL_OFFSET);
+  kio_printf("PDPT: index 1, val %x, ptr %x\n\n", pdpt[511].full_entry,
+             (size_t)&pdt[512] - KERNEL_OFFSET);
 
 #define MB2 0x200000
 
@@ -178,6 +183,9 @@ static void create_page_tables(void) {
   extern char _text_end[];
   const void *text_end = _text_end;
 
+  extern char _text[];
+  const void *text = _text;
+
   for (size_t i = 0; i < mb2s_needed; i++) {
     PT_entry_t *pt = safe_fmem_push();
     memset(pt, 0, PAGE_SIZE);
@@ -186,29 +194,55 @@ static void create_page_tables(void) {
     pdt[i].not_executable = 0;
     pdt[i].full_entry |= PAGE_ADDR(pt);
 
-    kio_printf("PDT: index %x, val %x\n", i, pdt[i].full_entry);
+    kio_printf("PDT: index %x, val %x, ptr %x\n", i, pdt[i].full_entry,
+               (size_t)pt - KERNEL_OFFSET);
 
     // 512 entries in a pt
     for (size_t j = 0; j < 512; j++) {
       size_t cur_addr = MB2 * i + PAGE_SIZE * j;
 
       pt[j].flags = PT_PRESENT;
-      if (cur_addr > (size_t)text_end - KERNEL_OFFSET) {
+      if (cur_addr > (size_t)text_end - KERNEL_OFFSET ||
+          cur_addr <= (size_t)text - KERNEL_OFFSET) {
         pt[j].flags |= PT_READ_WRITE;
         pt[j].not_executable = 1;
       } else {
         pt[j].not_executable = 0;
       }
+      // pt[j].not_executable = 0;
+      // pt[j].flags |= PT_READ_WRITE;
       pt[j].full_entry |= cur_addr & PAGE_TABLE_ENTRY_ADDR_MASK;
+
+      if (0xf17fa8 > cur_addr && 0xf17fa8 < cur_addr + PAGE_SIZE) {
+        kio_printf("Correct\n");
+      }
     }
   }
-
-  return;
 
   __asm__ volatile("movq %0, %%cr3"
                    :
                    : "r"((size_t)pml4 - KERNEL_OFFSET)
                    : "memory");
+
+  kio_printf("PML4 addr %x\n", (size_t)pml4 - KERNEL_OFFSET);
+
+  for (size_t i = 0; i < 5000; i++) {
+    outb(0x43, 0x30);
+    io_wait();
+    outb(0x40, 0xA9);
+    io_wait();
+    outb(0x40, 0x4);
+    io_wait();
+
+    bool dontStop = true;
+    while (dontStop) {
+      outb(0x43, 0xE2);
+      io_wait();
+      dontStop = !check_bit(inb(0x40), 7);
+    }
+  }
+
+  memset(NULL, 0, 3);
 }
 
 /// This function cannot call mmap or physical map or anything cuz like they
