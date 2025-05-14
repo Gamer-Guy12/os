2aec10a4f68bc6641b6b865177f09b6aa6516aff
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-05-14 02:39:22 +0000

    I have come to the realization that i may want to map virtual memory first

--- kernel/arch/x86_64/libk/kgfx/clear.c
+++ kernel/arch/x86_64/libk/kgfx/clear.c
@@ -4,7 +4,7 @@
 #include <x86_64.h>
 
 static volatile uint16_t *terminalBuffer =
-    (volatile uint16_t *)(0xb8000 + KERNEL_OFFSET);
+    (volatile uint16_t *)(0xb8000 + KERNEL_CODE_OFFSET);
 
 void kgfx_clear(void) {
   for (uint16_t i = 0; i < VGA_WIDTH; i++) {
