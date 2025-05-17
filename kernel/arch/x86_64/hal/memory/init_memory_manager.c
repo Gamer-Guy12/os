de66769324169c53fb6ce782a5aca3458ae88399
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-05-17 23:13:33 +0000

    Did some reverts

--- kernel/arch/x86_64/hal/memory/init_memory_manager.c
+++ kernel/arch/x86_64/hal/memory/init_memory_manager.c
@@ -26,6 +26,9 @@ void *kernel_end = end_kernel;
 
 size_t used_page_count = 0;
 
+#define PHYS_BUMP_START (void *)((uint8_t *)kernel_end - KERNEL_CODE_OFFSET)
+#define PAGE_ADDR(ptr) (((size_t)ptr - KERNEL_CODE_OFFSET) & 0x0007fffffffff000)
+
 #define NEXT_PAGE                                                              \
   (void *)(ROUND_UP((size_t)(uint8_t *)kernel_end, PAGE_SIZE) +                \
            used_page_count * PAGE_SIZE)
@@ -46,113 +49,112 @@ static void create_page_tables(void) {
   kio_printf("Address: %x and %x\n", (size_t)pml4 - KERNEL_CODE_OFFSET,
              (size_t)pdpt - KERNEL_CODE_OFFSET);
 
-  pml4[511].flags = PML4_USER_PAGE | PML4_READ_WRITE | PML4_PRESENT;
+  pml4[511].full_entry = PAGE_ADDR(pdpt);
   pml4[511].not_executable = 0;
-  pml4[511].full_entry |= PAGE_ADDR(pdpt);
+  pml4[511].flags = PML4_PRESENT | PML4_READ_WRITE;
 
-  kio_printf("PML4: index 0, val %x, ptr %x\n", pml4[511].full_entry,
-             (size_t)pdpt - KERNEL_OFFSET);
+  PDT_entry_t *pdt = NEXT_PAGE;
+  used_page_count += 2;
+  CLEAR_PAGE(pdt);
+  CLEAR_PAGE(&pdt[512]);
 
-  // Save 2 pages (hopefully contigouous)
-  // Should be cuz no other concurrent things
-  PDT_entry_t *pdt = safe_fmem_push();
-  safe_fmem_push();
-  memset(pdt, 0, PAGE_SIZE * 2);
-
-  pdpt[510].flags = PDPT_PRESENT | PDPT_READ_WRITE;
+  pdpt[510].full_entry = PAGE_ADDR(pdt);
   pdpt[510].not_executable = 0;
-  pdpt[510].full_entry |= PAGE_ADDR(pdt);
+  pdpt[510].flags = PDPT_PRESENT | PDPT_READ_WRITE;
 
-  pdpt[511].flags = PDPT_PRESENT | PDPT_READ_WRITE;
+  pdpt[511].full_entry = PAGE_ADDR(&pdt[512]);
   pdpt[511].not_executable = 0;
-  pdpt[511].full_entry |= PAGE_ADDR(&pdt[512]);
-
-  kio_printf("PDPT: index 0, val %x, ptr %x\n", pdpt[510].full_entry,
-             (size_t)pdt - KERNEL_OFFSET);
-  kio_printf("PDPT: index 1, val %x, ptr %x\n\n", pdpt[511].full_entry,
-             (size_t)&pdt[512] - KERNEL_OFFSET);
+  pdpt[511].flags = PDPT_PRESENT | PDPT_READ_WRITE;
 
 #define MB2 0x200000
 
+  extern char start_kernel[];
+  void *kernel_start = start_kernel;
+
   size_t mb2s_needed =
-      ROUND_UP((size_t)fmem_get_ptr(NULL) - KERNEL_OFFSET, MB2) / MB2 + 1;
-
-  kio_printf("MB2s: %x\n", mb2s_needed);
-
-  // extern char _text_end[];
-  // const void *text_end = _text_end;
-  //
-  // extern char _text[];
-  // const void *text = _text;
+      ROUND_UP((size_t)kernel_end - KERNEL_CODE_OFFSET, MB2) / MB2;
 
   for (size_t i = 0; i < mb2s_needed; i++) {
-    PT_entry_t *pt = safe_fmem_push();
-    memset(pt, 0, PAGE_SIZE);
+    PT_entry_t *pt = NEXT_PAGE;
+    used_page_count++;
+    CLEAR_PAGE(pt);
 
-    pdt[i].flags = PDT_PRESENT | PDT_READ_WRITE;
+    kio_printf("Addresses: %x, %x and the pt %x\n",
+               (size_t)pdt - KERNEL_CODE_OFFSET,
+               (size_t)&pdt[512] - KERNEL_CODE_OFFSET,
+               (size_t)pt - KERNEL_CODE_OFFSET);
+
+    pdt[i].full_entry = PAGE_ADDR(pt);
     pdt[i].not_executable = 0;
-    pdt[i].full_entry |= PAGE_ADDR(pt);
+    pdt[i].flags = PDT_PRESENT | PDT_READ_WRITE;
 
-    kio_printf("PDT: index %x, val %x, ptr %x\n", i, pdt[i].full_entry,
-               (size_t)pt - KERNEL_OFFSET);
-
-    // 512 entries in a pt
     for (size_t j = 0; j < 512; j++) {
-      size_t cur_addr = MB2 * i + PAGE_SIZE * j;
+      // The bios stuff is also there so ja
+      bool before_kernel =
+          MB2 * i + PAGE_SIZE * j < (size_t)kernel_start - KERNEL_CODE_OFFSET;
 
-      pt[j].flags = PT_PRESENT;
-
-      // TODO: add security for pages
-      // if (cur_addr >= (size_t)text_end - KERNEL_OFFSET ||
-      //     cur_addr < (size_t)text - KERNEL_OFFSET) {
-      //   pt[j].flags |= PT_READ_WRITE;
-      //   pt[j].not_executable = 1;
-      // } else {
-      //   pt[j].not_executable = 0;
-      // }
+      pt[j].full_entry = MB2 * i + PAGE_SIZE * j;
+      if (__builtin_expect(before_kernel, false)) {
+        pt[j].not_executable = 0;
+        pt[j].flags = PT_PRESENT;
+      } else {
+        pt[j].not_executable = 1;
+        pt[j].flags = PT_PRESENT | PT_READ_WRITE;
+      }
 
       pt[j].not_executable = 0;
-      pt[j].flags |= PT_READ_WRITE;
-      pt[j].full_entry |= cur_addr & PAGE_TABLE_ENTRY_ADDR_MASK;
-
-      if (0xf17fa8 > cur_addr && 0xf17fa8 < cur_addr + PAGE_SIZE) {
-        kio_printf("Correct\n");
-      }
+      pt[j].flags = PT_PRESENT | PT_READ_WRITE;
     }
   }
 
-  __asm__ volatile("movq %0, %%cr3"
+  // Recursive Paging
+  pml4[510].full_entry = PAGE_ADDR(pml4);
+  pml4[510].not_executable = 1;
+  pml4[510].flags = PT_PRESENT | PT_READ_WRITE;
+
+  __asm__ volatile("mov %0, %%cr3"
                    :
-                   : "r"((size_t)pml4 - KERNEL_OFFSET)
+                   : "r"((size_t)pml4 - KERNEL_CODE_OFFSET)
                    : "memory");
+}
 
-  kio_printf("PML4 addr %x\n", (size_t)pml4 - KERNEL_OFFSET);
+static void create_pages_for_physical_map(void) {
+  PDPT_entry_t *l3_table = NEXT_PAGE;
+  used_page_count++;
+  CLEAR_PAGE(l3_table);
 
-  for (size_t i = 0; i < 5000; i++) {
-    outb(0x43, 0x30);
-    io_wait();
-    outb(0x40, 0xA9);
-    io_wait();
-    outb(0x40, 0x4);
-    io_wait();
+  PML4_entry_t *pml4_entry = (PML4_entry_t *)PAGE_INDICES_TO_ADDR(
+      510ull, 510ull, 510ull, 510ull, 256ull * sizeof(PML4_entry_t));
 
-    bool dontStop = true;
-    while (dontStop) {
-      outb(0x43, 0xE2);
-      io_wait();
-      dontStop = !check_bit(inb(0x40), 7);
-    }
-  }
+  pml4_entry->full_entry = PAGE_ADDR(l3_table - KERNEL_CODE_OFFSET);
+  pml4_entry->not_executable = 1;
+  pml4_entry->flags = PML4_PRESENT | PML4_READ_WRITE;
 
-  memset(NULL, 0, 3);
+  // Clear the TLB and reset
+  __asm__ volatile(
+      "mov %0, %%cr3"
+      :
+      : "r"((size_t)PAGE_INDICES_TO_ADDR(510ull, 510ull, 510ull, 510ull, 0) -
+            KERNEL_CODE_OFFSET)
+      : "memory");
+}
+
+static void init_physical_map(void) {
+  // Im just creating up to the l3 page tables
+  create_pages_for_physical_map();
 }
 
 /// This function cannot call mmap or physical map or anything cuz like they
 /// depend on it being ready
 void init_memory_manager(void) {
-  fmem_init();
-
-  create_physical_map();
-
+  // Add Fmem if you want
   create_page_tables();
+
+  uint64_t *idk =
+      (uint64_t *)PAGE_INDICES_TO_ADDR(510ull, 0ull, 0ull, 0ull, 0ull);
+  *idk = 0;
+
+  return;
+
+  init_physical_map();
 }
--- kernel/include/hal/memory.h
+++ kernel/include/hal/memory.h
@@ -4,32 +4,20 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-typedef enum { phys_reserved, phys_free, phys_allocated } phys_mem_type_t;
+void *phys_alloc(void);
+void *phys_alloc_at(void *addr);
 
-struct phys_mem_region_struct {
-  void *base;
-  size_t len;
-  size_t pages;
-  struct phys_mem_region_struct *next;
-  struct phys_mem_region_struct *prev;
-  phys_mem_type_t type;
-};
+/// It should return NULL
+void *phys_free(void *addr);
 
-typedef struct phys_mem_region_struct phys_mem_region_t;
+void *virt_map_at(void *phys_addr, void *virt_addr);
 
-typedef struct {
-  void *base;
-  size_t size;
-  size_t region_count;
-  phys_mem_region_t *first;
-} phys_mem_section_t;
+/// Maps into kernel space
+void *virt_kernel_map(void *addr);
+/// Maps into user space
+void *virt_user_map(void *addr);
 
-void *phys_alloc(phys_mem_section_t *section);
-/// These are contiguous pages, just use phys_alloc multiple times to get
-/// multiple non-contiguous ones
-void *phys_multi_alloc(phys_mem_section_t *section, size_t page_count);
-void phys_reserve(phys_mem_section_t *section, size_t page_offset,
-                  size_t page_count);
-void phys_unreserve(phys_mem_region_t *region, phys_mem_section_t *section);
+/// Should return NULL
+void *virt_unmap(void *addr);
 
 #endif
