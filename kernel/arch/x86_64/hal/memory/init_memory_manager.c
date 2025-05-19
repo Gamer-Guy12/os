e52ab37364f17fae2e2aa5270dc6f74d3b38c9af
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-05-19 04:45:07 +0000

    Started testing on the higher half mapping may have done something wrong

--- kernel/arch/x86_64/hal/memory/init_memory_manager.c
+++ kernel/arch/x86_64/hal/memory/init_memory_manager.c
@@ -21,18 +21,18 @@ typedef struct {
   uint32_t reserved;
 } __attribute__((packed)) multiboot_memory_t;
 
-static void create_page_tables(void) {
-  // The page tables will be after the kernel but will then be remapped to be
-  // recursive page tables
+size_t used_page_count = 0;
 
-  size_t used_page_count = 0;
-
-  extern char end_kernel[];
-  void *kernel_end = end_kernel;
+extern char end_kernel[];
+void *kernel_end = end_kernel;
 
 #define NEXT_PAGE (void *)((uint8_t *)kernel_end + used_page_count * PAGE_SIZE)
 #define CLEAR_PAGE(ptr) memset(ptr, 0, PAGE_SIZE)
 #define PAGE_ADDR(ptr) (((size_t)ptr - KERNEL_CODE_OFFSET) & 0x0007fffffffff000)
+
+static void create_page_tables(void) {
+  // The page tables will be after the kernel but will then be remapped to be
+  // recursive page tables
 
   PML4_entry_t *pml4 = NEXT_PAGE;
   used_page_count++;
@@ -104,14 +104,17 @@ static void create_page_tables(void) {
   }
 
   // Recursive Paging
-  pml4[510].full_entry = PAGE_ADDR(pml4);
-  pml4[510].not_executable = 1;
-  pml4[510].flags = PT_PRESENT | PT_READ_WRITE;
+  PT_entry_t *recursive_entry = (PT_entry_t *)&pml4[510];
+  recursive_entry->full_entry = PAGE_ADDR((size_t)pml4);
+  recursive_entry->not_executable = 0;
+  recursive_entry->flags = PT_PRESENT | PT_READ_WRITE | PT_GLOBAL;
 
   __asm__ volatile("mov %0, %%cr3"
                    :
                    : "r"((size_t)pml4 - KERNEL_CODE_OFFSET)
                    : "memory");
+
+  kio_printf("ADDR: %x\n", (size_t)pml4 - KERNEL_CODE_OFFSET);
 }
 
 /// This function cannot call mmap or physical map or anything cuz like they
@@ -119,4 +122,36 @@ static void create_page_tables(void) {
 void init_memory_manager(void) {
   // Add Fmem if you want
   create_page_tables();
+
+  PDPT_entry_t *phys_3 = (PDPT_entry_t *)(GB * 3 + KB * 4);
+  PDPT_entry_t *phys_2 = (PDPT_entry_t *)(GB * 3 + KB * 8);
+  PDPT_entry_t *phys_1 = (PDPT_entry_t *)(GB * 3 + KB * 12);
+  PML4_entry_t *pml4 = (PML4_entry_t *)PML4_ADDR;
+  PDPT_entry_t *pdpt = (PDPT_entry_t *)PDPT_ADDR;
+  PDT_entry_t *pdt = (PDT_entry_t *)PDT_ADDR;
+  PT_entry_t *pt = (PT_entry_t *)PT_ADDR;
+
+  pml4[0].full_entry = PAGE_ADDR(phys_3 + KERNEL_CODE_OFFSET);
+  pml4[0].not_executable = 0;
+  pml4[0].flags = PML4_READ_WRITE | PML4_PRESENT;
+
+  pdpt[0].full_entry = PAGE_ADDR(phys_2 + KERNEL_CODE_OFFSET);
+  pdpt[0].not_executable = 0;
+  pdpt[0].flags = PDPT_PRESENT | PDPT_READ_WRITE;
+
+  pdt[0].full_entry = PAGE_ADDR(phys_1 + KERNEL_CODE_OFFSET);
+  pdt[0].not_executable = 0;
+  pdt[0].flags = PDT_PRESENT | PDT_READ_WRITE;
+
+  pt[0].full_entry = 0;
+  pt[0].not_executable = 0;
+  pt[0].flags = PT_PRESENT | PT_READ_WRITE;
+
+  __asm__ volatile("mov %0, %%cr3" : : "r"((size_t)0x1000000) : "memory");
+
+  uint64_t *num_ptr = 0x0;
+
+  *num_ptr = 4345;
+
+  kio_printf("Value stored wax %u\n", *num_ptr);
 }
