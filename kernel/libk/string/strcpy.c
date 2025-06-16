de87907d4cd08ec927a62c26d3b961421a59d35a
bb1uee <beavertonschooldistrict+blue@hotmail.com>
2025-06-15 17:53:07 -0700

    made b4etter

--- kernel/libk/string/strcpy.c
+++ kernel/libk/string/strcpy.c
@@ -1,9 +1,10 @@
 #include <libk/string.h>
 
 void strcpy(char *dest, const char *src) {
-    int i = -1;
-    while (src[i++]) {
+    int i = 0;
+    while (src[i]) {
         dest[i]=src[i];
+        i++;
     }
     dest[i]= '\0'
 }
