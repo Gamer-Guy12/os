edc60d20f7faefda6bd919fc3e73d299ddc163de
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-06-16 22:41:23 +0000

    I think i finished interrupt handling
    U register them and then u can call them

--- kernel/include/libk/err.h
+++ kernel/include/libk/err.h
@@ -8,7 +8,8 @@
 #define MEMORY_ALLOCATOR_ERR (0)
 #define MULTIBOOT_ERR (1ull << 48ull)
 #define MEMORY_INIT_ERR (2ull << 48ull)
+#define NO_INTERRUPT_HANDLER_ERR (3ull << 48ull)
 
-#define GET_CODE(full_err) (((full_err) & 0xFFFF) >> 48)
+#define GET_CODE(full_err) (((full_err) >> 48) & 0xFFFF)
 
 #endif
