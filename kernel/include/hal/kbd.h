8684272fc7d38a131611807e7aae43e674ac5514
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-07-02 00:27:11 +0000

    Made the keycode macros for qwerty (US)

--- kernel/include/hal/kbd.h
+++ kernel/include/hal/kbd.h
@@ -4,12 +4,84 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+// Keycodes
+#define QWERTY_KEY_ESC 0x0
+#define QWERTY_KEY_F1 0x2
+#define QWERTY_KEY_F2 0x3
+#define QWERTY_KEY_F3 0x4
+#define QWERTY_KEY_F4 0x5
+#define QWERTY_KEY_F5 0x7
+#define QWERTY_KEY_F6 0x8
+#define QWERTY_KEY_F7 0x9
+#define QWERTY_KEY_F8 0xA
+#define QWERTY_KEY_F9 0xB
+#define QWERTY_KEY_F10 0xC
+#define QWERTY_KEY_GRAVE (1 << 5)
+#define QWERTY_KEY_1 ((1 << 5) | 0x1)
+#define QWERTY_KEY_2 ((1 << 5) | 0x2)
+#define QWERTY_KEY_3 ((1 << 5) | 0x3)
+#define QWERTY_KEY_4 ((1 << 5) | 0x4)
+#define QWERTY_KEY_5 ((1 << 5) | 0x5)
+#define QWERTY_KEY_6 ((1 << 5) | 0x6)
+#define QWERTY_KEY_7 ((1 << 5) | 0x7)
+#define QWERTY_KEY_8 ((1 << 5) | 0x8)
+#define QWERTY_KEY_9 ((1 << 5) | 0x9)
+#define QWERTY_KEY_0 ((1 << 5) | 0xA)
+#define QWERTY_KEY_DASH ((1 << 5) | 0xB)
+#define QWERTY_KEY_EQUALS ((1 << 5) | 0xC)
+#define QWERTY_KEY_BACKSPACE ((1 << 5) | 0xD)
+#define QWERTY_KEY_TAB (2 << 5)
+#define QWERTY_KEY_Q ((2 << 5) | 0x1)
+#define QWERTY_KEY_W ((2 << 5) | 0x2)
+#define QWERTY_KEY_E ((2 << 5) | 0x3)
+#define QWERTY_KEY_R ((2 << 5) | 0x4)
+#define QWERTY_KEY_T ((2 << 5) | 0x5)
+#define QWERTY_KEY_Y ((2 << 5) | 0x6)
+#define QWERTY_KEY_U ((2 << 5) | 0x7)
+#define QWERTY_KEY_I ((2 << 5) | 0x8)
+#define QWERTY_KEY_O ((2 << 5) | 0x9)
+#define QWERTY_KEY_P ((2 << 5) | 0xA)
+#define QWERTY_KEY_OPEN_BRACKET ((2 << 5) | 0xB)
+#define QWERTY_KEY_CLOSE_BRACKET ((2 << 5) | 0xC)
+#define QWERTY_KEY_BACK_SLASH ((2 << 5) | 0xD)
+#define QWERTY_KEY_CAPS (3 << 5)
+#define QWERTY_KEY_A ((3 << 5) | 0x1)
+#define QWERTY_KEY_S ((3 << 5) | 0x2)
+#define QWERTY_KEY_D ((3 << 5) | 0x3)
+#define QWERTY_KEY_F ((3 << 5) | 0x4)
+#define QWERTY_KEY_G ((3 << 5) | 0x5)
+#define QWERTY_KEY_H ((3 << 5) | 0x6)
+#define QWERTY_KEY_J ((3 << 5) | 0x7)
+#define QWERTY_KEY_K ((3 << 5) | 0x8)
+#define QWERTY_KEY_L ((3 << 5) | 0x9)
+#define QWERTY_KEY_SEMICOLON ((3 << 5) | 0xA)
+#define QWERTY_KEY_QUOTE ((3 << 5) | 0xB)
+#define QWERTY_KEY_ENTER ((3 << 5) | 0xC)
+#define QWERTY_KEY_L_SHIFT (4 << 5)
+#define QWERTY_KEY_Z ((4 << 5) | 0x1)
+#define QWERTY_KEY_X ((4 << 5) | 0x2)
+#define QWERTY_KEY_C ((4 << 5) | 0x3)
+#define QWERTY_KEY_V ((4 << 5) | 0x4)
+#define QWERTY_KEY_B ((4 << 5) | 0x5)
+#define QWERTY_KEY_N ((4 << 5) | 0x6)
+#define QWERTY_KEY_M ((4 << 5) | 0x7)
+#define QWERTY_KEY_COMMA ((4 << 5) | 0x8)
+#define QWERTY_KEY_PERIOD ((4 << 5) | 0x9)
+#define QWERTY_KEY_SLASH ((4 << 5) | 0xA)
+#define QWERTY_KEY_R_SHIFT ((4 << 5) | 0xC)
+#define QWERTY_KEY_L_CTRL (5 << 5)
+#define QWERTY_KEY_WIN ((5 << 5) | 0x1)
+#define QWERTY_KEY_L_ALT ((5 << 5) | 0x2)
+#define QWERTY_KEY_SPACE ((5 << 5) | 0x3)
+#define QWERTY_KEY_R_ALT ((5 << 5) | 0x8)
+#define QWERTY_KEY_RN ((5 << 5) | 0xA)
+#define QWERTY_KEY_MENU ((5 << 5) | 0xB)
+#define QWERTY_KEY_R_CTRL ((5 << 5) | 0xC)
+
 typedef void (*ascii_key_down_handler)(uint8_t ascii);
 
 typedef struct {
-  bool (*check_ascii_down)(uint8_t key);
-  void (*register_ascii_handler)(ascii_key_down_handler handler);
-  void (*unregister_ascii_handler)(ascii_key_down_handler handler);
+  bool (*check_keycode_down)(uint8_t keycode);
 } hal_kbd_t;
 
 void hal_init_kbd(void);
--- planning/kbd.md
+++ planning/kbd.md
@@ -5,14 +5,14 @@ Scancodes:
 Organize into a grid, bits 0 - 4 contain the column, bits 5 - 8 contain row
 
 
-| | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | A | B | C |
-|-|-|-|- | - | - | - |-|-| -|-| - | -|-|
-| 0 | Esc | | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | F9 | F10 |
-| 1 | Grave | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |
-| 2 | Tab | Q | W | E | R | T | Y | U | I | O | P | [ | ] |
-| 3 | Caps | A | S | D | F | G | H | J | K | L | ; | ' | Enter |
-| 4 | L Shift | Z | X | C | V | B | N | M | , | . | / | | R Shift |
-| 5 | L Ctrl | Win | L Alt| Space | | | | | R Alt | | FN | Menu | R Ctrl |
+| | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | A | B | C | D |
+|-|-|-|- | - | - | - |-|-| -|-| - | -|-|-|
+| 0 | Esc | | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | F9 | F10 | |
+| 1 | Grave | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = | Bkspc |
+| 2 | Tab | Q | W | E | R | T | Y | U | I | O | P | [ | ] | \ |
+| 3 | Caps | A | S | D | F | G | H | J | K | L | ; | ' | Enter | |
+| 4 | L Shift | Z | X | C | V | B | N | M | , | . | / | | R Shift | |
+| 5 | L Ctrl | Win | L Alt| Space | | | | | R Alt | | FN | Menu | R Ctrl | |
 
 Ex:
 ```C
