e3ae8057a0f6a75028dbcd04094b663ca4733151
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-07-11 23:28:17 +0000

    I will do kmalloc after i do the process stuff

--- kernel/include/mem/kheap.h
+++ kernel/include/mem/kheap.h
@@ -1,26 +1,15 @@
 #ifndef KHEAP_H
 #define KHEAP_H
 
-#include <libk/mem.h>
-#include <mem/memory.h>
 #include <stddef.h>
-#include <stdint.h>
 
 #define KERNEL_REGION_PTR_LOCATION                                             \
   ((vmm_kernel_region_t **)(260ull * 512ull * GB + (0xffffull << 48)))
 
 typedef struct heap_entry_struct {
   size_t size;
-  union {
-    /// Points to a higher address, the start of the heap contains the
-    /// first entry and is searched upwards from there
-    struct heap_entry_struct *next;
-    struct {
-      uint64_t free : 1;
-      uint64_t ignored : 2;
-      uint64_t dont_use : 61;
-    };
-  };
+  struct heap_entry_struct *next;
+  struct heap_entry_struct *prev;
 } heap_entry_t;
 
 // Aligns the kernel heap to 8 bytes
